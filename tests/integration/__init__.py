c9413c68d4bc70c7627910bfeba26f57e7083d5d
Nahom2162002 <66095224+Nahom2162002@users.noreply.github.com>
2025-07-08 23:30:56 -0400

    Integration tests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,8 +1,25 @@
 #tests/integration/test_main.py
 
-from main import app 
+from main import *
 from fastapi import * 
 import pytest 
 
-def test_http_exception_handler(request: Request, exc: HTTPException):
-    
+def test_add_route(operation: OperationRequest):
+    result = add(operation.a, operation.b)
+
+    assert OperationResponse(result=result)
+
+def test_subtract_route(operation: OperationRequest):
+    result = subtract(operation.a, operation.b)
+
+    assert OperationResponse(result=result)
+
+def test_multiply_route(operation: OperationRequest):
+    result = multiply(operation.a, operation.b)
+
+    assert OperationResponse(result=result)
+
+def test_divide_route(operation: OperationRequest):
+    result = divide(operation.a, operation.b)
+
+    assert OperationResponse(result=result)
