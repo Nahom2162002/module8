0d3ee822e7a70fbfe7cd427811eea650dc000f78
Nahom2162002 <66095224+Nahom2162002@users.noreply.github.com>
2025-07-20 23:46:43 -0400

    Added tests for interaction

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,15 +2,30 @@
 
 import re 
 from playwright.sync_api import Page, expect 
+from main import * 
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("http://127.0.0.1:8000/")
 
     expect(page).to_have_title(re.compile("Playwright"))
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("http://127.0.0.1:8000/")
 
     page.get_by_role("link", name="Get started").click()
 
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+def test_number_input(page: Page):
+    page.goto("http://127.0.0.1:8000/")
+
+    page.get_by_role("textbox").fill("2")
+    expect(page.get_by_role("textbox")).to_be_visible()
+
+    page.get_by_role("textbox").fill("3")
+    expect(page.get_by_role("textbox")).to_be_visible()
+
+def test_operation_buttons(page: Page):
+    page.goto("http://127.0.0.1:8000/")
+
+    page.get_by_role("textbox").press("Left Click")
