a073a99b5cd47689385cad76be0b24d0cbc82b00
Nahom2162002 <66095224+Nahom2162002@users.noreply.github.com>
2025-07-23 00:01:15 -0400

    Pydantic schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,12 @@
+from typing import Optional 
+from uuid import UUID 
+from datetime import datetime 
+from pydantic import BaseModel, EmailStr, ConfigDict 
+
+class UserRead(BaseModel):
+    id: UUID 
+    username: str 
+    email: EmailStr 
+    created_at: datetime 
+
+    model_config = ConfigDict(from_attributes=True)
